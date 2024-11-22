b593e49ad5f6ee6904e635b0ccbe567f8872b033
bubuzi <bubuzi@88.com>
2024-11-22 09:56:22 +0800

    style: 调整preload/index.d.ts的类型命名

--- src/preload/index.d.ts
+++ src/preload/index.d.ts
@@ -1,20 +1,19 @@
-import { ElectronAPI } from '@electron-toolkit/preload'
+import { ElectronAPI } from '@electron-toolkit/preload';
 
-type HandleLike = typeof import('../main/ipc/handles')
-type D = keyof HandleLike
-type C<T extends D> = keyof HandleLike[T]
-type E = {
-  [key1 in D]: {
-    [key in C<key1>]: (
-      ...agrs: Exclude<Parameters<HandleLike[key1][key]>, import('electron').IpcMainInvokeEvent>
-    ) => Promise<ReturnType<HandleLike[key1][key]>>
-  }
-}
-const d: E = {}
+type HandleLike = typeof import('../main/ipc/handles');
+type HandleLikeServices = keyof HandleLike;
+type HandleLikeServiceFunction<T extends HandleLikeServices> = keyof HandleLike[T];
+type HandleApi = {
+  [ServiceName in HandleLikeServices]: {
+    [ServiceFunctionName in HandleLikeServiceFunction<ServiceName>]: (
+      ...agrs: Exclude<Parameters<HandleLike[ServiceName][ServiceFunctionName]>, import('electron').IpcMainInvokeEvent>
+    ) => Promise<ReturnType<HandleLike[ServiceName][ServiceFunctionName]>>;
+  };
+};
 
 declare global {
   interface Window {
-    electron: ElectronAPI
-    api: E
+    electron: ElectronAPI;
+    api: HandleApi;
   }
 }
