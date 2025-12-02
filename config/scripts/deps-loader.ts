edbaa7543cdaf38ccffaa585ec64f01e46a31298
bubuzi <bubuzi@88.com>
2025-12-02 11:20:10 +0800

    修复: 在使用字节加密时出现循环依赖导致引入错误的问题

--- config/scripts/deps-loader.ts
+++ config/scripts/deps-loader.ts
@@ -2,24 +2,33 @@ import fs from 'fs';
 import path from 'path';
 import Module from 'module';
 
+// 判断当前加载环境是否是由用户编写的代码环境，通过路径判断
+const IS_USER_CTX = /[\/\\]resources[\/\\]app(?:\.asar)?[\/\\]out[\/\\]main/i;
+
 const RESOURCE_PATH = path.join(path.parse(process.execPath).dir, 'resources');
 const DEP_PATHS = fs
   .readdirSync(RESOURCE_PATH)
   .filter((item) => item.endsWith('.asar') && item.startsWith('deps'))
   .map((item) => path.join(RESOURCE_PATH, item));
-// @ts-ignore
-const ORIGIN_MODULE_LOADER = Module._load;
-// @ts-ignore
-Module._load = function (request, parent, isMain) {
-  const module = require.cache[request];
-  if (module && module.exports) {
-    return module.exports;
-  }
-  for (const DEP_PATH of DEP_PATHS) {
-    if (!parent.paths.includes(DEP_PATH)) {
-      parent.paths.push(DEP_PATH);
+// @ts-ignore 存储默认的库路径解析函数
+const originalResolve = Module._resolveFilename;
+// @ts-ignore 重写库路径加载
+Module._resolveFilename = function (request, parent, isMain, options) {
+  if (parent && Array.isArray(parent.paths)) {
+    for (const depPath of DEP_PATHS) {
+      const nodeModulesPath = path.join(depPath);
+      if (!parent.paths.includes(nodeModulesPath)) {
+        parent.paths.unshift(nodeModulesPath); // unshift 比 push 优先级更高
+      }
     }
   }
-  const depModule = ORIGIN_MODULE_LOADER.apply(this, [request, parent, isMain]);
-  return depModule;
+  // parentId代表当前请求是从哪个文件发出
+  const parentId = parent?.id;
+  if (parentId && IS_USER_CTX.test(parentId) && request.startsWith('./') && request.endsWith('.js')) {
+    // 发出文件是jsc格式时替换
+    if (parentId.endsWith('.jsc')) {
+      request = request + 'c';
+    }
+  }
+  return originalResolve.call(this, request, parent, isMain, options);
 };
